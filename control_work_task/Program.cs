3be77ae6c0d8042a6220332da59a087ebd495f17
HemlockK <2345serega5432@mail.ru>
2022-11-30 08:48:09 +0900

    task complete

---
+++ control_work_task/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+
+string[] array = AskArray();
+string[] result = FindLess(array, 3);
+WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] FindLess(string[] input, int n) 
+{
+    string[] output = new string[CountLess(input, n)];
+
+    for(int i = 0, a = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= n) {
+            output[a] = input[i];
+            a++;
+        }
+    }
+
+    return output;
+}
+
+int CountLess(string[] input, int n) 
+{
+    int count = 0;
+    for(int i = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= n) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] AskArray() 
+{
+    Write("Введите значения через пробел: ");
+    return ReadLine().Split(" ");
+}
